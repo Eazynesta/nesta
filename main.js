26057f566e9c87513726037d48870f52517e9875
eazynesta <nestaamwangi@gmail.com>
2024-06-25 09:36:10 +0300

    Checkpoint

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-var typd= new typed(".text",{
-    String:["Frontend Developer","Youtuber","Web Developer"],
-    typespeed:100,
+var typed = new Typed(".text",{
+    strings:["Frontend Developer","Youtuber","Web Developer"],
+    typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
     loop:true
 })
+
